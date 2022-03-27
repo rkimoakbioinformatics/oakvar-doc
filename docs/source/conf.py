2af52dc874eb5527b58a2940fd87a4500cb47e1a
Rick Kim <13386712+rkimoakbioinformatics@users.noreply.github.com>
2022-03-28 04:03:19 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OakCRAVAT'
+copyright = '2022, Ryangguk Kim'
+author = 'Ryangguk Kim'
 
 release = '0.1'
 version = '0.1.0'
